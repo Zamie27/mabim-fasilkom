f026bf97d9412d7fb405e4ed968229ea10d95c2b
Zamie <zamie693@gmail.com>
2023-11-07 10:29:28 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function Login() {
   var done = 0;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username == "Admin" && password == "admin") {
+    if (confirm("Apakah anda ingin login?") == true) {
+      window.location.href = "https://kuukok.site/";
+    } else {
+      alert("Login dibatalkan !");
+    }
+  } else {
+    if (done == 0) {
+      alert("Username/Password Salah");
+    }
+  }
 }
